b00bf1ca22121294d2d19857fe547d83f4101044
Pat Pragman <pat@pragman.io>
2024-01-10 17:38:12 -0900

    added labels

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request, redirect, abort
 import urllib.request
-import hashlib
+import datetime
 import json
 
 url = "http://api.open-notify.org/iss-now.json"
@@ -9,12 +9,13 @@ app = Flask(__name__)
 
 pat_lats = [61.217381]
 pat_lons = [-149.863129]
+pat_times = [datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")]
 
 @app.route('/update_pat',
            methods=['POST']
            )
 def update_pat():
-    global pat_lats, pat_lons
+    global pat_lats, pat_lons, pat_times
     with open('access.txt', "r") as file:
         stored_creds = file.read()
 
@@ -22,12 +23,15 @@ def update_pat():
     # Compare the hashed password with stored credentials
     if password == stored_creds:
         pat_lat, pat_lon = float(request.form['latitude']), float(request.form['longitude'])
+        pat_time = request.form['timestamp']
         pat_lats.append(pat_lat)
         pat_lons.append(pat_lon)
+        pat_times.append(pat_time)
 
         while len(pat_lons) > 10:
             pat_lons.pop(0)
             pat_lats.pop(0)
+            pat_times.pop(0)
     else:
         # If they do not match, return a 401 Unauthorized error
         abort(401)  # This will stop the function and return the error to the client
@@ -40,8 +44,10 @@ def positions():
     positions = json.loads(
         urllib.request.urlopen(url).read()
     )
+    iss_info_timestamp = datetime.datetime.utcnow()
+    positions['iss_position']["timestamp"] = iss_info_timestamp.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
-    positions['pat_position'] = {"latitude":pat_lats[-1], "longitude":pat_lons[-1]}
+    positions['pat_position'] = {"latitude":pat_lats[-1], "longitude":pat_lons[-1], "timestamp": pat_times[-1]}
     return jsonify(positions)
 
 
