f1c7f8fff1794ccecb9776efddce2f9e3fefcb57
Pat Pragman <pat@pragman.io>
2024-01-10 15:38:56 -0900

    minor update

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ url = "http://api.open-notify.org/iss-now.json"
 
 app = Flask(__name__)
 
-pat_lat, pat_lon = 61.217381, -149.863129
-
+pat_lats = [61.217381]
+pat_lons = [-149.863129]
 
 @app.route('/update_pat',
            methods=['POST']
            )
 def update_pat():
-    global pat_lat, pat_lon
+    global pat_lats, pat_lons
     with open('access.txt', "r") as file:
         stored_creds = file.read()
 
@@ -22,6 +22,12 @@ def update_pat():
     # Compare the hashed password with stored credentials
     if password == stored_creds:
         pat_lat, pat_lon = float(request.form['latitude']), float(request.form['longitude'])
+        pat_lats.append(pat_lat)
+        pat_lons.append(pat_lon)
+
+        while len(pat_lons) > 10:
+            pat_lons.pop(0)
+            pat_lats.pop(0)
     else:
         # If they do not match, return a 401 Unauthorized error
         abort(401)  # This will stop the function and return the error to the client
