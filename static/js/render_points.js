6076ab4f85d59306065b222f386c2fd20bfb5b1d
Pat Pragman <pat@pragman.io>
2024-01-23 15:39:48 -0900

    changed the map markers so they don't always update every time

--- static/js/render_points.js
+++ static/js/render_points.js
@@ -1,16 +1,11 @@
 // initiate the map, center it on anchorage
-var map = L.map('map').setView([61.217381, -149.863129], 13);
-var markers = [];
-
+const MAXITEMS = 10;
+const map = L.map('map').setView([61.217381, -149.863129], 13);
+let markers = [];  // store all the markers in this
 // Add a tile layer to add to our map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
-
-function zip(arr1, arr2) {
-    return arr1.map((k, i) => [k, arr2[i]]);
-}
-
 
 // Add a pat marker to the map
 const pat_icon = L.icon({
@@ -21,12 +16,37 @@ const pat_icon = L.icon({
 });
 
 
-// Add a pat marker to the map
+// Add a dot marker to the map
 const dot_icon = L.icon({
     iconUrl: "/image/dot.png",
     iconSize: [16, 16],
     iconAnchor: [8, 8],
 });
+
+class MarkerPoint {
+    // define the marker point class we'll build
+    constructor(lat, lon, timestamp) {
+        this.latitude = lat;
+        this.longitude = lon;
+        this.timestamp = timestamp;
+        this.marker = L.marker([this.latitude, this.longitude], {icon:dot_icon});
+        this.popupContent = `<h3>Position: ${this.latitude}, ${this.longitude}</h3>
+<p>Timestamp: ${this.timestamp}</p>`;
+    }
+
+    equals(otherMarkerPoint) {
+        return otherMarkerPoint instanceof MarkerPoint &&
+                this.latitude === otherMarkerPoint.latitude &&
+                this.longitude === otherMarkerPoint.longitude &&
+                this.timestamp === otherMarkerPoint.timestamp;
+    }
+}
+
+
+function zip(arr1, arr2) {
+    return arr1.map((k, i) => [k, arr2[i]]);
+}
+
 
 // Function to update the marker position
 function update_markers() {
@@ -36,12 +56,6 @@ function update_markers() {
         })
         .then(function(data) {
 
-
-
-            markers.forEach(function(marker){
-                map.removeLayer(marker);
-            });
-
             let coordinates = zip(zip(data.pat_position.latitudes, data.pat_position.longitudes), data.pat_position.timestamps);
 
 
@@ -49,20 +63,28 @@ function update_markers() {
                 let lat = coord[0][0];
                 let lon = coord[0][1];
                 let timeStamp = coord[1];
-                let popupContent = `
-                    <h3>Position: ${lat}, ${lon}</h3>
-                    <p>Timestamp: ${timeStamp}</p>
-                `;
+                let marker = new MarkerPoint(lat, lon, timeStamp);
+                if (!markers.some(otherMarker => marker.equals(otherMarker))){
+                    // if the marker isn't in the list, add it to the list, then add it to the map, and bind
+                    // a popup to it
+                    markers.push(marker);
+                    marker.marker.addTo(map);
+                    marker.marker.bindPopup(marker.popupContent);
+                }
+            })
 
-                var marker = L.marker([lat, lon], {icon:dot_icon}).addTo(map);
-                marker.bindPopup(popupContent);
-                markers.push(marker);
+            markers.forEach(function (marker){
+                marker.marker.setIcon(dot_icon);
             })
 
             // change the last icon
             if (markers.length > 0) {
-                console.log("here!")
-                markers[markers.length - 1].setIcon(pat_icon);
+                markers[markers.length - 1].marker.setIcon(pat_icon);
+            }
+
+            // now get rid of all the items less than MAXITEMS
+            while (markers.length > MAXITEMS){
+                markers.shift().marker.remove()
             }
 })};
 
